9bfd4f96c9e345214c939ca9031ee31971333751
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2017-12-01 20:44:17 -0500

    Add New Item on ListView

---
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/AnthonyActivity.java
@@ -0,0 +1,13 @@
+package crats.mvcbaseproject.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AnthonyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anthony);
+    }
+}
