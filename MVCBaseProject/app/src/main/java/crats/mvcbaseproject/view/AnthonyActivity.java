8a38e99843ee5d830e6d69f8087f82df74632674
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2017-12-08 17:58:55 -0500

    Add a Test Button to Activity

--- MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/AnthonyActivity.java
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/AnthonyActivity.java
@@ -2,6 +2,10 @@ package crats.mvcbaseproject.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import crats.mvcbaseproject.R;
 
 public class AnthonyActivity extends AppCompatActivity {
 
@@ -9,5 +13,20 @@ public class AnthonyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anthony);
+        Button btn = (Button) findViewById(R.id.btn);
+
+//        btn.bind("<Button>")
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    void click() {}
+
+    public void ASU(View view) {
+
     }
 }
