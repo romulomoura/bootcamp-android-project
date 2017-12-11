2efe1fc6f67c378f466d844af0decebc7a8a7e4d
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2017-12-11 16:18:50 -0500

    Rename layout name

--- MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/MovieListActivity.java
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/MovieListActivity.java
@@ -2,8 +2,6 @@ package crats.mvcbaseproject.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import crats.mvcbaseproject.R;
 
@@ -12,21 +10,8 @@ public class MovieListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie_list);
-        Button btn = (Button) findViewById(R.id.btn);
-
-//        btn.bind("<Button>")
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-    }
-
-    void click() {}
-
-    public void ASU(View view) {
+        setContentView(R.layout.activity_movies_list);
 
     }
+
 }
