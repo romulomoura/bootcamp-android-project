4c58f16ea0416318544fc855f137acc61bc01c0a
ANTHONY44-PC\Anthony <anthony.salim.123@gmail.com>
2017-12-11 19:21:20 -0500

    Work on Movie List Activity

--- MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/MovieListActivity.java
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/MovieListActivity.java
@@ -2,16 +2,57 @@ package crats.mvcbaseproject.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 import crats.mvcbaseproject.R;
+import crats.mvcbaseproject.controller.IMovieController;
+import crats.mvcbaseproject.controller.MovieController;
+import crats.mvcbaseproject.model.Movie;
 
-public class MovieListActivity extends AppCompatActivity {
+public class MovieListActivity extends AppCompatActivity implements AdapterView.OnItemClickListener, IMovieController{
+    ListView movieListView;
+    ArrayList<Movie> moviesList = new ArrayList<Movie>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies_list);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        movieListView = (ListView) findViewById(R.id.movieoptionsListView);
+
+        MovieController.shared().setupMovieController(this, getBaseContext());
+    }
+    private void setupListView() {
+        ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this,R.layout.listview_movie_detail, moviesList);
+        movieListView.setOnItemClickListener(this);
+        movieListView.setAdapter(adapter);
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Movie movie = MovieController.shared().getMoviesList ().get(position);
+        Toast.makeText(this, movie.getTitle(), Toast.LENGTH_SHORT).show();
+    }
+
+    public ArrayList<Movie> getMoviesList() {
+        return MovieController.shared().getMoviesList();
+    }
+
+    @Override
+    public void fetchSuccess() {
+        moviesList = this.getMoviesList();
+        this.setupListView();
+    }
+
+    @Override
+    public void fetchFailure(String errorMessage) {
+
+    }
 }
