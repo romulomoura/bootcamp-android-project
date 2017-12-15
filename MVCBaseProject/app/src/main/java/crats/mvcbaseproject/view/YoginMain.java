9f44effb18eb498854deec97b3ada0993e5061b1
Yogin Bhatt <yogin.bhattce@gmail.com>
2017-12-15 15:20:39 -0500

    Main activity optimized

--- MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/YoginMain.java
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/YoginMain.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.text.TextUtils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +29,141 @@ import android.widget.Toast;
 public class YoginMain extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+    private Button loginButton;
+    private Button createActButton;
+    private EditText emailField;
+    private EditText passwordField;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.yoginmain);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginButton = (Button) findViewById(R.id.login);
+       // createActButton = (Button) findViewById(R.id.loginCreateAccount);
+        emailField = (EditText) findViewById(R.id.emailEd);
+        passwordField = (EditText) findViewById(R.id.passwordEd);
+
+/*        createActButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             //   startActivity(new Intent(YoginMain.this, CreateAccountActivity.class));
+                finish();
+            }
+        });
+*/
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                mUser = firebaseAuth.getCurrentUser();
+
+                if (mUser != null) {
+                    Toast.makeText(YoginMain.this, "Signed In", Toast.LENGTH_LONG).show();
+                //    startActivity(new Intent(YoginMain.this, PostListActivity.class));
+                    finish();
+                }else {
+                    Toast.makeText(YoginMain.this, "Not Signed In", Toast.LENGTH_LONG).show();
+                }
+
+
+
+
+            }
+        };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(emailField.getText().toString())
+                        && !TextUtils.isEmpty(passwordField.getText().toString())) {
+
+                    String email = emailField.getText().toString();
+                    String pwd = passwordField.getText().toString();
+
+                    login(email, pwd);
+
+
+
+
+                }else {
+
+                }
+            }
+        });
+
+
+
+    }
+
+    private void login(String email, String pwd) {
+
+        mAuth.signInWithEmailAndPassword(email, pwd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+                            //Yay!! We're in!
+                            Toast.makeText(YoginMain.this, "Signed in", Toast.LENGTH_LONG)
+                                    .show();
+
+                         //   startActivity(new Intent(YoginMain.this, PostListActivity.class));
+                            finish();
+                        }else {
+                            // Not it!
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        if (item.getItemId() == R.id.action_signout) {
+
+            mAuth.signOut();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+
+
+   /* private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     private static final String TAG = "YoginMain";
     private EditText email;
@@ -72,6 +210,8 @@ login.setOnClickListener(new View.OnClickListener() {
     public void onClick(View view) {
         final String emailString = email.getText().toString();
         String pwd = password.getText().toString();
+        Log.i(TAG, "onClick: ");
+
 
         if(!emailString.equals("") && !pwd.equals(""))
         {
@@ -154,4 +294,9 @@ login.setOnClickListener(new View.OnClickListener() {
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
+    */
+
+
+
+
 }
