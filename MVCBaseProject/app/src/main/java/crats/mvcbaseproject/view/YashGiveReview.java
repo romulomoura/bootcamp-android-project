8c1c08575c2a9154fd35f7892930249c78ed38c2
shahyash9795 <shahyash9795@gmail.com>
2017-12-16 02:23:11 -0500

    Some Animation

--- MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/YashGiveReview.java
+++ MVCBaseProject/app/src/main/java/crats/mvcbaseproject/view/YashGiveReview.java
@@ -1,10 +1,15 @@
 package crats.mvcbaseproject.view;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -20,7 +25,12 @@ public class YashGiveReview extends AppCompatActivity {
     EditText review,productname;
     Button submit;
 
+    ProgressBar progressBar;
     String qrcode = "";
+
+
+    private int mProgressstatus = 0;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +41,86 @@ public class YashGiveReview extends AppCompatActivity {
         productname = (EditText)findViewById(R.id.productname);
         review = (EditText) findViewById(R.id.review);
         submit = (Button) findViewById(R.id.submit);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar2);
 
 
         Bundle sum = getIntent().getExtras();
         qrcode = sum.getString("qrcode");
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        submit.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v)
+            {
+
+                String getEditText = productname.getText().toString();
+                String getEdiText1 = review.getText().toString();
 
 
-                Getset getset = new Getset();
+                if (TextUtils.isEmpty(getEditText))
+                {
+                    Toast.makeText(getApplicationContext(), "Please Enter Product Name",
+                            Toast.LENGTH_LONG).show();
+                }
+                else if (TextUtils.isEmpty(getEdiText1))
+                {
+                    Toast.makeText(getApplicationContext(), "Please Enter Product Review",
+                            Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    submit.setVisibility(View.INVISIBLE);
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            while (mProgressstatus < 100) {
+                                mProgressstatus++;
+                                android.os.SystemClock.sleep(10);
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        progressBar.setProgress(mProgressstatus);
 
-                getset.setQrcode(qrcode);
-                getset.setReview(review.getText().toString());
-                getset.setProductName(productname.getText().toString());
+                                    }
+                                });
+                            }
+                            mHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+
+                                    Toast.makeText(getApplicationContext(), "Review Submitted",
+                                            Toast.LENGTH_LONG).show();
+                                    Intent intentyash = new Intent(YashGiveReview.this, FirstScreen.class);
+                                    startActivity(intentyash);
+
+                                }
+                            });
+
+                        }
+                    }).start();
 
 
-                FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference databaseReference = database.getReference();
-                databaseReference.child("primarydatabase").push().setValue(getset);
+                    Getset getset = new Getset();
 
-                Toast.makeText(getApplicationContext(), "Review Submitted",
-                        Toast.LENGTH_LONG).show();
-
+                    getset.setQrcode(qrcode);
+                    getset.setReview(review.getText().toString());
+                    getset.setProductName(productname.getText().toString());
 
 
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference databaseReference = database.getReference();
+                    databaseReference.child("primarydatabase").push().setValue(getset);
 
+
+                }
 
 
             }
+
+
+
+
+
 
 
         });
